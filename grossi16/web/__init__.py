054fc6c325065cfab47ec5a1bbd3e6e6d153329e
Gabriel Queiroz <gabrieljvnq@gmail.com>
2016-08-26 16:56:20 -0300

    Começo do parte web

--- grossi16/web/__init__.py
+++ grossi16/web/__init__.py
@@ -8,15 +8,41 @@ __maintainer__ = "Gabriel Queiroz"
 __email__ = "gabrieljvnq@gmail.com"
 __status__ = "Pre-alpha"
 
-from flask import Flask
-webapp = Flask("Grossi16")
+import click
+import logging
+import tempfile
+import flask
+
+webapp = flask.Flask("Grossi16")
 
 @webapp.route("/")
 def hello():
     return "Hello World!"
 
-def main():
-    webapp.run(host='0.0.0.0', port=8080)
+@click.command()
+@click.option(
+    '--port',
+    '-p',
+    default=8080,
+    type=click.IntRange(0, 65535),
+    help='Port in which the web server will listen to'
+)
+@click.option(
+    '--bind-address',
+    'addr',
+    '-a',
+    default="0.0.0.0",
+    help='Address in which to bind the web server. Leave the default if you do not know what you are doing!'
+)
+@click.option(
+    '--code',
+    '-c',
+    default="1234",
+    type=click.IntRange(0, 65535),
+    help="Teacher's console password"
+)
+def main(addr, port, code):
+    webapp.run(host=addr, port=port)
 
 if __name__ == "__main__":
     main()
