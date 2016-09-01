d4589006c7bcc6c84cda98d6b872a4bb561628e1
Gabriel Queiroz <gabrieljvnq@gmail.com>
2016-09-01 11:43:07 -0300

    Removida interface gráfica do setup.py. Espero que isso seja apenas provisório.

--- setup.py
+++ setup.py
@@ -8,12 +8,11 @@ setup(name='grossi16',
       author_email='gabrieljvnq@gmail.com',
       license='MIT',
       package_dir={'gorssi16': 'grossi16'},
-      package_data={'grossi16.gui': ['*.ui'], 'grossi.web': ['static/*.css', 'static/*.js', 'static/*.woff2', 'templates/*.html']},
-      packages=['grossi16.gui', 'grossi16.web', 'grossi16.cli'],
+      package_data={'grossi.web': ['static/*.css', 'static/*.js', 'static/*.woff2', 'templates/*.html']},
+      packages=['grossi16.web', 'grossi16.cli'],
       zip_safe=True,
       include_package_data=True,
       install_requires=[
-        'pygubu==0.9.7.7',
         'flask==0.11.1',
         'click==6.6',
         'CommonMark==0.7.2',
@@ -21,10 +20,9 @@ setup(name='grossi16',
       ],
       entry_points={
           'console_scripts': [
-              'grossi16=grossi16.gui:main',
               'grossi16-cli=grossi16.cli:main'
           ],
           'setuptools.installation': [
-              'eggsecutable=grossi16.gui:main'
+              'eggsecutable=grossi16.cli:main'
           ],
       })
